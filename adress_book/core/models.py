87d55af39490779c2b4935bb4ee207635932cf67
anton-serozhechkin <serezhechkintosh@gmail.com>
2020-07-23 04:40:11 +0300

    add model record, add to admin panel, createsuperuser, update gitignore

--- adress_book/core/models.py
+++ adress_book/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django_countries.fields import CountryField
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class Record(models.Model):
+    name = models.CharField('Имя', max_length=100, unique=True)
+    last_name = models.CharField('Фамилия', max_length=100, unique=True)
+    country = CountryField('Страна', default='UA', blank=True, null=True)
+    city = models.CharField('Город', max_length=100, blank=True, null=True)
+    street = models.CharField('Улица', max_length=100, blank=True, null=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Телефонный номер должен быть в формате: '+999999999'. Допускается до 15 цифр.")
+    phone_number = models.CharField('Телефонный номер', validators=[phone_regex], max_length=17) # валидатор должен быть списком    URL (опциональное поле, с валидацией) 
+    image = models.ImageField('Фотография', upload_to='images/%Y/%m/%h/', null=True, blank=True)
+    
+    class Meta:
+        verbose_name = 'Запись в адресной книге'
+        verbose_name_plural = 'Записи в адресной книге'
+    
+    def __str__(self):
+        if self.country:
+            return 'Пользователь {} {} из {}.'.format(self.last_name, self.name, self.country.name)
+        else:
+            return 'Пользователь {} {}.'.format(self.last_name, self.name)
+
+    #def get_absolute_url(self):
+    #    return reverse('event_detail', kwargs={'slug': self.slug})
